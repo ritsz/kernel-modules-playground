3f8e2bc4f379e08bdc090b782ddbb7fefd22e7b3
ritsz <r.ranjan789@gmail.com>
2013-07-27 17:31:59 +0530

    swapper details

--- thread_tree.c
+++ thread_tree.c
@@ -1,19 +1,27 @@
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>  // for task_struct
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-int hello_init(void)
+int __init hello_init(void)
 {
 	struct task_struct *task, *g, *t;
 	printk("ENTERING THREAD TREE MODULE\n");	
 	
+
+	//	Information about the swapper(IDLE)
+	printk("Process %d : %s has %d threads\n", init_task.pid, init_task.comm, get_nr_threads(&init_task));
+	printk("%lu time, %llu wait, %llu last run and %llu last queued\n\n", init_task.sched_info.pcount, init_task.sched_info.run_delay,
+			init_task.sched_info.last_arrival, init_task.sched_info.last_queued);
+	
 	// 	Shouldn't be used too often, takes too long.
 	//	Prints the name, no. of threads and scheduling info of all process
 	// 	using the list_head list.
+
 	for_each_process(task) {
-		printk("Process : %s has %d threads\n", task->comm, get_nr_threads(task));
+		printk("Process %d : %s has %d threads\n", task->pid, task->comm, get_nr_threads(task));
 		printk("%lu time, %llu wait, %llu last run and %llu last queued\n\n", task->sched_info.pcount, task->sched_info.run_delay,
 				task->sched_info.last_arrival, task->sched_info.last_queued);
 	}
@@ -25,7 +33,7 @@ int hello_init(void)
 	return 0;
 }
 
-static void hello_exit(void)
+static __exit void hello_exit(void)
 {
 	printk("EXITING THREAD TREE MODULE\n");
 }
