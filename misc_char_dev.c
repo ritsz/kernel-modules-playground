cf0b95f5462e8efe0d9b03e1b1c63b843759a331
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-06-08 22:32:24 +0530

    adding read/write locks to misc_char_dev

--- misc_char_dev.c
+++ misc_char_dev.c
@@ -4,24 +4,32 @@
 #include <linux/uaccess.h>
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
+#include <linux/spinlock.h>
 
 #define MAXBUFF 128
+DEFINE_RWLOCK(my_rwlock);
 static char kern_buff[MAXBUFF];		/*Kernel Buffer to hold user data*/
 static char my_eud_id[12] = "HELLO WORLD\0"; /*string*/
 static ssize_t my_read(struct file *f, char __user *buf,
 		size_t len, loff_t *off)
 {
+	int ret;
 	dump_stack();
-	return simple_read_from_buffer(buf, len, off, my_eud_id, strlen(my_eud_id));
+	read_lock(&my_rwlock);
+	ret = simple_read_from_buffer(buf, len, off, my_eud_id, strlen(my_eud_id));
+	read_unlock(&my_rwlock);
+	return ret;
 }
 
 static ssize_t my_write(struct file *f, const char __user *buf,
 		    size_t len, loff_t *off)
 {
 	dump_stack();
+	write_lock(&my_rwlock);
 	int ret = simple_write_to_buffer(kern_buff, MAXBUFF, off, buf, len);
 	kern_buff[ret] = '\0';
-	
+	write_unlock(&my_rwlock);
+
 	if (strcmp(kern_buff, "linux") != 0)
 		return -EINVAL;		/* Return Invalid Error for String*/
 	else
