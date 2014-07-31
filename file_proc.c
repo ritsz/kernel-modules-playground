86f6810ee69d9a5bb1c1956948ba5fa94949da15
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-07-31 18:14:46 +0530

    Coding standard changes to file_proc.c

--- file_proc.c
+++ file_proc.c
@@ -17,65 +17,64 @@ static struct proc_dir_entry *simacc_file;
 static int simacc_read(char *page, char **start, off_t off,
     int count, int *eof, void *data)
 {
-    int len;
-    len = sprintf(page, "%s\n", result);
-    return len;
+    	int len;
+    	len = sprintf(page, "%s\n", result);
+    	return len;
 }
 
 static int simacc_write(struct file *file, const char *buffer,
     unsigned long count, void *data)
 {
-    int len;
-    unsigned long num = 0, sum = 0;
-    int i;
-    char c;
-    if (count > MAX_INPUT_SIZE)
-    	len = MAX_INPUT_SIZE;
-    else
-    	len = count;
+    	int len;
+    	unsigned long num = 0, sum = 0;
+    	int i;
+    	char c;
+    	if (count > MAX_INPUT_SIZE)
+    		len = MAX_INPUT_SIZE;
+    	else
+    		len = count;
 
-    if(copy_from_user(input_buf, buffer, len))
-    	return -EFAULT;
-    input_buf[len] = '&#92;&#48;';
+    	if(copy_from_user(input_buf, buffer, len))
+    		return -EFAULT;
+    	input_buf[len] = '&#92;&#48;';
 
-    i = 0;
-    do {
-    	c = input_buf[i++];
-    	if (c >= '0' && c <= '9') {
-        	num = num * 10 + c - '0';
-    	} else {
-        	sum += num;
-    	   	num = 0;
-    	}
-    } while (c != '&#92;&#48;' && i < MAX_INPUT_SIZE);
+    	i = 0;
+    	do {
+    		c = input_buf[i++];
+    		if (c >= '0' && c <= '9') {
+        		num = num * 10 + c - '0';
+    		} else {
+        		sum += num;
+    		   	num = 0;
+    		}
+    	} while (c != '&#92;&#48;' && i < MAX_INPUT_SIZE);
     
-    sprintf(result, "%lu", sum);
+    	sprintf(result, "%lu", sum);
     
-    return len;
+    	return len;
 } 
 
 static int __init init_simacc(void) {
-    simacc_file = create_proc_entry("simacc", 0666, NULL);
-    if (simacc_file == NULL) {
-    return -ENOMEM;
-    }
+    	simacc_file = create_proc_entry("simacc", 0666, NULL);
+    	if (simacc_file == NULL)
+    		return -ENOMEM;
 
-    printk(KERN_ALERT,"Current pid is %d : %s\n", current->pid, current->comm);
+    	pr_info("Current pid is %d : %s\n", current->pid, current->comm);
 
-    input_buf[MAX_INPUT_SIZE - 1] = '&#92;&#48;';
-    result[0] = '0'; result[1] = '&#92;&#48;';
-    simacc_file->data = input_buf;
-    simacc_file->read_proc = simacc_read;
-    simacc_file->write_proc = simacc_write;
-    printk(KERN_ALERT"%s: Module Loaded.\n", __this_module.name);
+    	input_buf[MAX_INPUT_SIZE - 1] = '&#92;&#48;';
+    	result[0] = '0'; result[1] = '&#92;&#48;';
+    	simacc_file->data = input_buf;
+    	simacc_file->read_proc = simacc_read;
+    	simacc_file->write_proc = simacc_write;
+    	pr_info("%s: Module Loaded.\n", __this_module.name);
 
-    return 0;
+    	return 0;
 }
 
 static void __exit cleanup_simacc(void)
 {
-    remove_proc_entry("simacc", NULL);
-    printk(KERN_INFO "%s: Module Unloaded.\n", THIS_MODULE->name);
+    	remove_proc_entry("simacc", NULL);
+    	printk(KERN_INFO "%s: Module Unloaded.\n", THIS_MODULE->name);
 }
 
 module_init(init_simacc);
