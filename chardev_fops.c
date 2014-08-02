0d5723c17daeec4560b860ec115c282ebeb88eef
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-08-02 19:28:57 +0530

    last

--- chardev_fops.c
+++ chardev_fops.c
@@ -1,5 +1,4 @@
 #include <linux/init.h>
-//#include <linux/autoconf.h>
 #include <linux/module.h>
 #include <linux/kernel.h> /* printk() */
 #include <linux/slab.h> /* kmalloc() */
@@ -10,8 +9,8 @@
 #include <linux/fcntl.h> /* O_ACCMODE */
 #include <asm/system.h> /* cli(), *_flags */
 #include <asm/uaccess.h> /* copy_from/to_user */
-MODULE_LICENSE("Dual BSD/GPL");
 
+MODULE_LICENSE("Dual BSD/GPL");
 
 /* Declaration of memory.c functions */
 int memory_open(struct inode *inode, struct file *filp);
@@ -21,17 +20,12 @@ ssize_t memory_write(struct file *filp, char *buf, size_t count, loff_t *f_pos);
 void memory_exit(void);
 int memory_init(void);
 
-/* Structure that declares the usual file */
-/* access functions */
 struct file_operations memory_fops = {
 	read: memory_read,
 	write: memory_write,
 	open: memory_open,
 	release: memory_release
 };
-/* Declaration of the init and exit functions */
-module_init(memory_init);
-module_exit(memory_exit);
 /* Global variables of the driver */
 /* Major number */
 int memory_major = 60;
@@ -107,3 +101,6 @@ ssize_t memory_write( struct file *filp, char *buf,size_t count, loff_t *f_pos) 
 	copy_from_user(memory_buffer,tmp,1);
 	return 1;
 }
+
+module_init(memory_init);
+module_exit(memory_exit);
